9ea60d2b60133909ad100852c0e5cd5a93ab40a7
Kauann-00 <marcoskauan2007@gmail.com>
2025-05-13 00:46:31 -0300

    Update JogoPalavraEmbaralhada.java

--- JogoPalavraEmbaralhada/src/main/java/com/mycompany/jogopalavraembaralhada/JogoPalavraEmbaralhada.java
+++ JogoPalavraEmbaralhada/src/main/java/com/mycompany/jogopalavraembaralhada/JogoPalavraEmbaralhada.java
@@ -1,4 +1,4 @@
-// 
+// string
 
 package com.mycompany.jogopalavraembaralhada;
 
@@ -13,19 +13,18 @@ public class JogoPalavraEmbaralhada {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         
-        // Lista de palavras
+        
         String[] palavras = {
             "JAVA", "PROGRAMA", "COMPUTADOR", "ALGORITMO", 
             "DESENVOLVIMENTO", "CODIGO", "SISTEMA"
         };
         
-        // Escolhe uma palavra aleatoriamente
         String palavraCorreta = palavras[random.nextInt(palavras.length)];
         
-        // Embaralha a palavra
+        
         String palavraEmbaralhada = embaralharPalavra(palavraCorreta);
         
-        // Inicializa variáveis do jogo
+        
         int tentativasMaximas = 6;
         int tentativas = 0;
         boolean acertou = false;
@@ -34,7 +33,7 @@ public class JogoPalavraEmbaralhada {
         System.out.println("A palavra embaralhada é: " + palavraEmbaralhada);
         System.out.println("Você tem " + tentativasMaximas + " tentativas para adivinhar a palavra.");
         
-        // Loop do jogo
+       
         while (tentativas < tentativasMaximas && !acertou) {
             System.out.print("Tentativa " + (tentativas + 1) + ": Digite sua palavra: ");
             String tentativa = scanner.nextLine().trim().toUpperCase();
@@ -48,7 +47,7 @@ public class JogoPalavraEmbaralhada {
             }
         }
         
-        // Exibe o resultado final
+     
         System.out.println("\nFim do jogo!");
         System.out.println("A palavra correta era: " + palavraCorreta);
         if (acertou) {
